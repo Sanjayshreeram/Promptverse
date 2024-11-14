972f937aa9089c634bea1427a16c5d62d0cd669c
Your Name <sanjayshreeram77@gmail.com>
2024-11-14 11:57:59 +0530

    added ui

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,28 +1,56 @@
-import Link from "next/link";
+// /components/Navbar.tsx
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+//import { Icons } from "@/components/icons"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import { Calendar } from "@/components/ui/calendar"
+
+// Define the navigation items here
+const navItems = [
+  { title: "Home", href: "/" },
+  { title: "Sign In", href: "/api/auth/signin" },
+  { title: "Sign Out", href: "/api/auth/signout" },
+  { title: "Server", href: "/server" },
+  { title: "Client", href: "/client" },
+  { title: "Extra", href: "/extra" },
+]
 
 export default function Navbar() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
-    <nav className="bg-blue-800 p-4">
-      <ul className="flex justify-evenly text-2xl font-bold">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/api/auth/signin">Sign In</Link>
-        </li>
-        <li>
-          <Link href="/api/auth/signout">Sign Out</Link>
-        </li>
-        <li>
-          <Link href="/server">Server</Link>
-        </li>
-        <li>
-          <Link href="/client">Client</Link>
-        </li>
-        <li>
-          <Link href="/extra">Extra</Link>
-        </li>
-      </ul>
+    <nav className="bg-transparent flex items-center justify-center m-auto p-4">
+      <NavigationMenu>
+        <NavigationMenuList className="flex justify-evenly text-2xl font-bold">
+          {navItems.map((item) => (
+            <NavigationMenuItem key={item.title}>
+              <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul>
+                  <li>
+                    <Link href={item.href} legacyBehavior passHref>
+                      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        {item.title}
+                      </NavigationMenuLink>
+                    </Link>
+                  </li>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+      
     </nav>
-  );
+  )
 }
